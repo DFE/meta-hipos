9eff2e3e501e18cfe0c5e3df76fd9f82c235ad92
Eik Binschek <binschek@dresearch-fe.de>
2015-03-24 14:17:06 +0100

    network: add link down link up to fix network problem HYP-11185
    add new ethernet link down link up on system boot
    
    Signed-off-by: Eik Binschek <binschek@dresearch-fe.de>

--- recipes-hipos/hipos-network-setup/files/hipos-network-setup.sh
+++ recipes-hipos/hipos-network-setup/files/hipos-network-setup.sh
@@ -23,9 +23,15 @@ do
                 then
                         echo "eth$i: use 1000MBit/s"
                         ethtool -s eth$i advertise 0xFF
+                        ifconfig eth$i down
+                        sleep 1
+                        ifconfig eth$i up
                 else
                         echo "eth$i: use 100MBit/s"
                         ethtool -s eth$i advertise 0xF
+                        ifconfig eth$i down
+                        sleep 1
+                        ifconfig eth$i up
         fi
 done
 
