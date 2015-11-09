c3480deabb29165c7a11e07981c78b36f203946a
Eik Binschek <binschek@dresearch-fe.de>
2015-11-09 12:00:06 +0100

    hipos-system-tuning: add memory garbage collection and ISR-CPU-core shift HYP-12921
    Signed-off-by: Eik Binschek <binschek@dresearch-fe.de>

---
+++ recipes-hipos/hipos-system-tuning/files/hipos-system-tuning.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# get CPU-Core count
+cores="`cat /proc/cpuinfo | /bin/grep processor | /usr/bin/wc -l`"
+
+# shift ISRs to other CPU-core if 4 cores (0-3) are available
+if [ ${cores} -eq 4 ]
+	then
+		/bin/echo "reconfigure: smp_affinity"
+		# network to core 3
+		/bin/echo 8 >/proc/irq/150/smp_affinity
+		/bin/echo 8 >/proc/irq/151/smp_affinity
+		# IPU and VPU to core 2
+		/bin/echo 4 >/proc/irq/35/smp_affinity
+		/bin/echo 4 >/proc/irq/37/smp_affinity
+		/bin/echo 4 >/proc/irq/38/smp_affinity
+		/bin/echo 4 >/proc/irq/39/smp_affinity
+		/bin/echo 4 >/proc/irq/40/smp_affinity
+		/bin/echo 4 >/proc/irq/44/smp_affinity
+		# PCIe and SATA to core 1
+		/bin/echo 2 >/proc/irq/71/smp_affinity
+		/bin/echo 2 >/proc/irq/155/smp_affinity
+fi
+
+while [ true ]
+do
+	/bin/sleep 10
+	# use "sync" to get free buffers
+	/bin/cat /proc/buddyinfo | awk '{ if( ($(NF-1)+(($NF)*2)) < 1 ){printf("%s-%s:\t", $3,$4); print("sync"); system("/bin/sync");} }'
+	/bin/sleep 10
+	# compact memory to get more big continuous free memory  
+	/bin/cat /proc/buddyinfo | awk '{ if( ($(NF-1)+(($NF)*2)) < 8 ){printf("%s-%s:\t", $3,$4); print("compact_memory"); system("/bin/echo 1 >/proc/sys/vm/compact_memory");} }'
+	/bin/sleep 10
+	# drop caches to collect unused buffers
+	/bin/cat /proc/buddyinfo | awk '{ if( ($(NF-1)+(($NF)*2)) < 4 ){printf("%s-%s:\t", $3,$4); print("drop_caches"); system("/bin/echo 3 > /proc/sys/vm/drop_caches");} }'
+	/bin/sleep 10
+	# compact memory to get more big continuous free memory  
+	/bin/cat /proc/buddyinfo | awk '{ if( ($(NF-1)+(($NF)*2)) < 8 ){printf("%s-%s:\t", $3,$4); print("compact_memory"); system("/bin/echo 1 >/proc/sys/vm/compact_memory");} }'
+done
+
---
+++ recipes-hipos/hipos-system-tuning/hipos-system-tuning.bb
@@ -0,0 +1,28 @@
+DESCRIPTION = "This systemd-service is used to setup network phys"
+SECTION = "base"
+LICENSE = "GPLv2"
+LIC_FILES_CHKSUM = " file://../COPYING;md5=9ac2e7cff1ddaf48b6eab6028f23ef88 "
+
+inherit systemd allarch
+
+RDEPENDS_${PN} = " ethtool bash "
+
+PR = "r1"
+
+SRC_URI = " file://hipos-system-tuning.service  \
+            file://hipos-system-tuning.sh \
+	    file://COPYING "
+
+FILES_${PN} = "${base_libdir}/systemd \
+               ${sysconfdir}/hipos/hipos-system-tuning.sh \
+		"
+
+SYSTEMD_SERVICE_${PN} = "hipos-system-tuning.service"
+
+do_install () {
+  install -d ${D}${base_libdir}/systemd/system
+  install -m 0644 ${WORKDIR}/hipos-system-tuning.service ${D}${base_libdir}/systemd/system/
+  install -d ${D}${sysconfdir}/hipos
+  install -m 0755 ${WORKDIR}/hipos-system-tuning.sh ${D}${sysconfdir}/hipos/
+}
+
