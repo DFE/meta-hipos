52df5bcd9ce061c5d662b536c8c0e337c7e79f77
Mario Schuknecht <mario.schuknecht@dresearch-fe.de>
2016-01-29 10:11:18 +0100

    hipos-network-setup.sh: Fix indentation
    Signed-off-by: Mario Schuknecht <mario.schuknecht@dresearch-fe.de>

--- recipes-hipos/hipos-network-setup/files/hipos-network-setup.sh
+++ recipes-hipos/hipos-network-setup/files/hipos-network-setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [ -a /etc/hydraip-devid ]
-        then
-                . /etc/hydraip-devid
+then
+	. /etc/hydraip-devid
 fi
 
 if [ -z ${lanspeed} ]
-        then
-	        lanspeed=ff
-	else
+then
+	lanspeed=ff
+else
 	if [ ${#lanspeed} = 1 ]
-		then
-			lanspeed=${lanspeed}f
+	then
+		lanspeed=${lanspeed}f
 	fi
 fi
 
@@ -19,23 +19,23 @@ echo "lanspeed=$lanspeed"
 
 for (( i=0; i<${#lanspeed}; i++ ))
 do
-        if [ "${lanspeed:$i:1}" == "g" ]
-                then
-                        echo "eth$i: use 1000MBit/s"
-                        ethtool -s eth$i advertise 0xFF
-                else
-                        echo "eth$i: use 100MBit/s"
-                        ethtool -s eth$i advertise 0xF
-        fi
+	if [ "${lanspeed:$i:1}" == "g" ]
+	then
+		echo "eth$i: use 1000MBit/s"
+		ethtool -s eth$i advertise 0xFF
+	else
+		echo "eth$i: use 100MBit/s"
+		ethtool -s eth$i advertise 0xF
+	fi
 done
 
 if [ -a "/etc/hip-activate-config.d/${device}/net_config.py" ]
-        then
-                /etc/hip-activate-config.d/${device}/net_config.py
-                if [ -a /etc/drconfig/post_config.sh ]
-                        then
-                                rm /etc/drconfig/post_config.sh
-                        fi
+then
+	/etc/hip-activate-config.d/${device}/net_config.py
+	if [ -a /etc/drconfig/post_config.sh ]
+	then
+	rm /etc/drconfig/post_config.sh
+	fi
 fi
 
 
