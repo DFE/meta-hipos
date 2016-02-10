5a77ab6937fa3dc43ea56742518f9015cb475af3
Angelika Tobisch <tobisch@dresearch-fe.de>
2016-02-10 15:58:04 +0100

    hipos-net-check: enforce consistency of lanspeed as well, HYP-13287
    This was renamed from hipos-mac-check which only considered
    the MAC address.
    
    Signed-off-by: Angelika Tobisch <tobisch@dresearch-fe.de>

---
+++ recipes-hipos/hipos-net-check/files/hipos-net-check.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+""" Himx only:
+    Compare mac address and lanspeed parameters in file created from board controller info with the one in the uboot
+    environment (which is on SD card and thus may be transferred from one device to another one).
+
+    The one in uboot environment is modified, if necessary.
+
+    Is supposed to run when booting _after_ the file on HD has been updated with the info from the board controller.
+"""
+
+import subprocess
+import sys
+import re
+
+
+OK_CODE = 0
+ERROR_CODE = 1
+
+
+def main():
+    devidfile = "/etc/hydraip-devid"
+    devidcontent = read_file(devidfile)
+    ubootcontent = subprocess.check_output("fw_printenv", shell=True)
+
+    parameters = [ "ethaddr", "lanspeed" ]
+
+    for p in parameters:
+        devidvalue = re.search(r'^{}=(.+)$'.format(p), devidcontent, flags=re.MULTILINE)
+        if devidvalue:
+            # force lower case of all letters to avoid detecting ab:cd... as different from AB:CD...
+            devidvalue = devidvalue.group(1).lower()
+            ubootvalue = re.search(r'^{}=(.+)$'.format(p), ubootcontent, flags=re.MULTILINE)
+            if ubootvalue:
+                ubootvalue = ubootvalue.group(1).lower()
+            if devidvalue != ubootvalue:
+                subprocess.check_output("fw_setenv {} {}".format(p, devidvalue), shell=True)
+
+
+def read_file(file):
+    """ Read the content of a file.
+
+        Exit program with ERROR_CODE, if an error was encountered.
+        Return content, otherwise
+
+        :param file: file to be displayed
+        :returns: the content of the file
+    """
+    try:
+        with open(file) as f:
+            return f.read().rstrip()
+    except IOError as e:
+        sys.exit(OK_CODE)
+
+
+if __name__ == '__main__': # pragma: no cover
+    main()
---
+++ recipes-hipos/hipos-net-check/hipos-net-check.bb
@@ -0,0 +1,27 @@
+DESCRIPTION = "This systemd-service is used to enforce consistency of network settings in board controller and in uboot."
+SECTION = "base"
+LICENSE = "GPLv2"
+LIC_FILES_CHKSUM = " file://../COPYING;md5=9ac2e7cff1ddaf48b6eab6028f23ef88 "
+
+inherit systemd allarch
+
+RDEPENDS_${PN} = "python"
+
+PR = "r1"
+
+SRC_URI = " file://hipos-net-check.service  \
+            file://hipos-net-check.py \
+	          file://COPYING "
+
+FILES_${PN} = "${base_libdir}/systemd \
+               ${sysconfdir}/hipos/hipos-net-check.py \
+		"
+
+SYSTEMD_SERVICE_${PN} = "hipos-net-check.service"
+
+do_install () {
+  install -d ${D}${base_libdir}/systemd/system
+  install -m 0644 ${WORKDIR}/hipos-net-check.service ${D}${base_libdir}/systemd/system/
+  install -d ${D}${sysconfdir}/hipos
+  install -m 0755 ${WORKDIR}/hipos-net-check.py ${D}${sysconfdir}/hipos/
+}
