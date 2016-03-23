46b76a2517c08928bf37b578af855fe4badd8d9d
Ralf Schröder <schroeder@dresearch-fe.de>
2016-03-23 13:07:34 +0100

    HYP-14029 script quality improved (shell checker)
    Signed-off-by: Ralf Schröder <schroeder@dresearch-fe.de>

--- recipes-hipos/hipos-system-tuning/files/hipos-system-tuning.sh
+++ recipes-hipos/hipos-system-tuning/files/hipos-system-tuning.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # get CPU-Core count
-cores="`cat /proc/cpuinfo | /bin/grep processor | /usr/bin/wc -l`"
+cores="$(/bin/grep processor /proc/cpuinfo | /usr/bin/wc -l)"
 
 # shift ISRs to other CPU-core if 4 cores (0-3) are available
-if [ ${cores} -eq 4 ]
+if [ "${cores}" -eq 4 ]
 	then
 		/bin/echo "reconfigure: smp_affinity"
 		# network to core 3
@@ -25,16 +25,16 @@ fi
 
 wait_sec=0;
 
-while [ true ]
+while true
 do
 	/bin/sleep $((wait_sec))
-	buffers_count="`/bin/cat /proc/buddyinfo | awk '{ print ($(NF-2)+($(NF-1)*2)+(($NF)*4)) }' | head -n 1`";
+	buffers_count="$(awk '{ print ($(NF-2)+($(NF-1)*2)+(($NF)*4)) }' < /proc/buddyinfo | head -n 1)";
 	wait_sec=$((buffers_count/4))
 	
 	# use "sync" to get free buffers
-	/bin/cat /proc/buddyinfo | awk '{ if( ($(NF-2)+($(NF-1)*2)+(($NF)*4)) < 4 ){printf("%s-%s:\t", $3,$4); print("sync"); system("/bin/sync");} }'
+	awk '{ if( ($(NF-2)+($(NF-1)*2)+(($NF)*4)) < 4 ){printf("%s-%s:\t", $3,$4); print("sync"); system("/bin/sync");} }' < /proc/buddyinfo
 	# drop caches to collect unused buffers
-	/bin/cat /proc/buddyinfo | awk '{ if( ($(NF-2)+($(NF-1)*2)+(($NF)*4)) < 8 ){printf("%s-%s:\t", $3,$4); print("drop_caches"); system("/bin/echo 3 > /proc/sys/vm/drop_caches");} }'
+	awk '{ if( ($(NF-2)+($(NF-1)*2)+(($NF)*4)) < 8 ){printf("%s-%s:\t", $3,$4); print("drop_caches"); system("/bin/echo 3 > /proc/sys/vm/drop_caches");} }' < /proc/buddyinfo
 	# compact memory to get more big continuous free memory  
-	/bin/cat /proc/buddyinfo | awk '{ if( ($(NF-2)+($(NF-1)*2)+(($NF)*4)) < 8 ){printf("%s-%s:\t", $3,$4); print("compact_memory"); system("/bin/echo 1 >/proc/sys/vm/compact_memory");} }'
+	awk '{ if( ($(NF-2)+($(NF-1)*2)+(($NF)*4)) < 8 ){printf("%s-%s:\t", $3,$4); print("compact_memory"); system("/bin/echo 1 >/proc/sys/vm/compact_memory");} }' < /proc/buddyinfo
 done
