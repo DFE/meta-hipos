38205032aba641eb14acdd6094f4fabc2e72ba59
Eik Binschek <binschek@dresearch-fe.de>
2016-04-15 14:45:55 +0200

    hipos-time: fix HIPOS open time service HYP-12310
    Signed-off-by: Eik Binschek <binschek@dresearch-fe.de>

--- recipes-hipos/hipos-time/files/set-time.sh
+++ recipes-hipos/hipos-time/files/set-time.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-date -u -s "$(drbcc --cmd=getrtc | cut -d\  -f2,3)" ||
+date -u -s "$(drbcc --dev=/dev/ttydrbcc --cmd=getrtc | cut -d\  -f2,3)" ||
 {  # in case of problems we log the commands
     set -x
     for try in 1 2 3 4 5; do
-      date -u -s "$(drbcc --cmd=getrtc | cut -d\  -f2,3)" && break
+      date -u -s "$(drbcc --dev=/dev/ttydrbcc --cmd=getrtc | cut -d\  -f2,3)" && break
     done 2>&1 | logger -t $0
     set -x
 }
