388e5631988494b72a218b5d75b44b1faa7b45f1
Eik Binschek <binschek@dresearch-fe.de>
2016-07-13 12:17:08 +0200

    HIPOS: faster poweroff on Ignition: off
    fix start limit of systemd watchdog timer service
    
    Signed-off-by: Eik Binschek <binschek@dresearch-fe.de>

--- recipes-hipos/hipos-watchdog/files/hipos-watchdog.sh
+++ recipes-hipos/hipos-watchdog/files/hipos-watchdog.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+if [ -a /var/run/lock/hipos-watchdog.lock ]; then
+	exit
+else
+/bin/touch /var/run/lock/hipos-watchdog.lock
 /usr/bin/drbcc --dev /dev/ttydrbcc --cmd gets | grep "Ignition: off" >/dev/null
 if [  $? -eq 0 ]; then
 	/bin/systemctl --no-block poweroff
+	exit
 else
 	/usr/bin/drbcc --dev /dev/ttydrbcc --cmd="heartbeat 180"
+fi
+/bin/rm /var/run/lock/hipos-watchdog.lock
 fi
 
