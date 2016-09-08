a06ef71a22a8eee9f3f3133c43e746761dacc16b
Sascha Dierberg <dierberg@dresearch-fe.de>
2016-09-08 12:46:46 +0200

    fast ntp time sync for dvmon added HYP-15451

--- recipes-hipos/hipos-time/files/set-time.sh
+++ recipes-hipos/hipos-time/files/set-time.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
-date -u -s "$(drbcc --dev=/dev/ttydrbcc --cmd=getrtc | cut -d\  -f2,3)" ||
-{  # in case of problems we log the commands
-    set -x
-    for try in 1 2 3 4 5; do
-      date -u -s "$(drbcc --dev=/dev/ttydrbcc --cmd=getrtc | cut -d\  -f2,3)" && break
-    done 2>&1 | logger -t $0
-    set -x
+
+MACHINE_TYPE="$(/usr/sbin/hip-machinfo -a)"
+SUB_TYPE="$(/usr/sbin/hip-machinfo -s)"
+VARIANT=$(. /etc/hydraip-devid; echo "${device:-recorder}")
+
+ntpserver_config() {
+        local lw
+        if [ -f "/etc/drconfig/$MACHINE_TYPE-$VARIANT/topology.json" ]; then
+                lw=$(jsonhelper -f "/etc/drconfig/$MACHINE_TYPE-$VARIANT/topology.json" -i LW)
+        else
+                lw=1
+        fi
+        jq -c -r .LW$lw.system.ntpserver "/etc/drconfig/$MACHINE_TYPE-$VARIANT/hydraip.json"
 }
+
+if [ "$SUB_TYPE" == "dvmon" ]; then
+        ntpserver=$(ntpserver_config) &&
+        if [ -n "$ntpserver" ]; then
+                ntpd -gq $ntpserver
+        fi
+else
+        date -u -s "$(drbcc --dev=/dev/ttydrbcc --cmd=getrtc | cut -d\  -f2,3)" ||
+        {  # in case of problems we log the commands
+                set -x
+                for try in 1 2 3 4 5; do
+                date -u -s "$(drbcc --dev=/dev/ttydrbcc --cmd=getrtc | cut -d\  -f2,3)" && break
+                done 2>&1 | logger -t $0
+                set -x
+        }
+fi
 
