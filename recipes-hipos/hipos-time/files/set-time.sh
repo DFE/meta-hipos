07611a28a3c469015df508c721babd8133922c0a
dierberg <dierberg@dresearch-fe.de>
2017-04-10 16:32:58 +0200

    Update set-time.sh
    config stuff removed from open source script HYP-16985

--- recipes-hipos/hipos-time/files/set-time.sh
+++ recipes-hipos/hipos-time/files/set-time.sh
@@ -4,27 +4,7 @@ MACHINE_TYPE="$(/usr/sbin/hip-machinfo -a)"
 SUB_TYPE="$(/usr/sbin/hip-machinfo -s)"
 VARIANT=$(. /etc/hydraip-devid; echo "${device:-recorder}")
 
-ntpserver_config() {
-        local lw
-        if [ -f "/etc/drconfig/$MACHINE_TYPE-$VARIANT/topology.json" ]; then
-                lw=$(jsonhelper -f "/etc/drconfig/$MACHINE_TYPE-$VARIANT/topology.json" -i LW)
-        else
-                lw=1
-        fi
-        jq -c -r .LW$lw.system.ntpserver "/etc/drconfig/$MACHINE_TYPE-$VARIANT/hydraip.json"
-}
-
-if [ "$SUB_TYPE" == "dvmon" ]; then
-        ntpserver=$(ntpserver_config) &&
-        if [ -n "$ntpserver" ]; then
-                if ping -c1 -W1 $ntpserver 2>&1 > /dev/null ; then
-                        if ntpq -p $ntpserver 2>&1 > /dev/null ; then
-                                ntpd -gq $ntpserver &&
-				touch /run/ntpd-synced
-                        fi
-                fi
-        fi
-else
+if [ "$SUB_TYPE" != "dvmon" ]; then
         date -u -s "$(drbcc --dev=/dev/ttydrbcc --cmd=getrtc | cut -d\  -f2,3)" ||
         {  # in case of problems we log the commands
                 set -x
