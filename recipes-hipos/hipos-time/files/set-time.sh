dd22d1c55039016dc5adc47a2c24bf389ca21b8f
Mario Schuknecht <mario.schuknecht@dresearch-fe.de>
2017-07-27 15:17:24 +0200

    No rtc on ipcam boards HYP-17274
    ipcam boards do not have a rtc.
    
    Signed-off-by: Mario Schuknecht <mario.schuknecht@dresearch-fe.de>

--- recipes-hipos/hipos-time/files/set-time.sh
+++ recipes-hipos/hipos-time/files/set-time.sh
@@ -4,7 +4,7 @@ MACHINE_TYPE="$(/usr/sbin/hip-machinfo -a)"
 SUB_TYPE="$(/usr/sbin/hip-machinfo -s)"
 VARIANT=$(. /etc/hydraip-devid; echo "${device:-recorder}")
 
-if [ "$SUB_TYPE" != "dvmon" ]; then
+if [ "$SUB_TYPE" != "dvmon" -a "$SUB_TYPE" != "ipcam" ]; then
         date -u -s "$(drbcc --dev=/dev/ttydrbcc --cmd=getrtc | cut -d\  -f2,3)" ||
         {  # in case of problems we log the commands
                 set -x
