ffd92a015b63838444523803e56828afc0db2e4f
Mario Schuknecht <mario.schuknecht@dresearch-fe.de>
2021-09-02 13:54:25 +0200

    hipos-vmstat: Show boot time
    The boot time is often useful when analyzing log data.
    
    Signed-off-by: Mario Schuknecht <mario.schuknecht@dresearch-fe.de>

--- recipes-hipos/hipos-vmstat/files/hipos-vmstat.sh
+++ recipes-hipos/hipos-vmstat/files/hipos-vmstat.sh
@@ -5,6 +5,9 @@ wait_sec=30;
 while true
 do
 	/bin/sleep $((wait_sec))
+	up_since=$(/usr/bin/uptime -s | tr -d "\n")
+	up_pretty=$(/usr/bin/uptime -p | tr -d "\n")
+	/usr/bin/logger -t vmstat "System up since $up_since $up_pretty"
 	/usr/bin/vmstat | /usr/bin/logger -t vmstat
 	/bin/cat /proc/buddyinfo | /usr/bin/logger -t vmstat
 done
