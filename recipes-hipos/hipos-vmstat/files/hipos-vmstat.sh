6aa08c0d377d938ac0f29bffa1a4c30fce4c948b
Mario Schuknecht <mario.schuknecht@dresearch-fe.de>
2021-09-02 15:43:46 +0200

    hipos-vmstat: Show firmware version
    The firmware version is often useful when analyzing log data.
    
    Signed-off-by: Mario Schuknecht <mario.schuknecht@dresearch-fe.de>

--- recipes-hipos/hipos-vmstat/files/hipos-vmstat.sh
+++ recipes-hipos/hipos-vmstat/files/hipos-vmstat.sh
@@ -5,6 +5,7 @@ wait_sec=30;
 while true
 do
 	/bin/sleep $((wait_sec))
+	/usr/bin/lsb_release -d | /usr/bin/logger -t vmstat
 	up_since=$(/usr/bin/uptime -s | tr -d "\n")
 	up_pretty=$(/usr/bin/uptime -p | tr -d "\n")
 	/usr/bin/logger -t vmstat "System up since $up_since $up_pretty"
