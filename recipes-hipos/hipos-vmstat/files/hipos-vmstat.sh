4162d93d52370751c284db10445e5cea96a9269c
Mario Schuknecht <mario.schuknecht@dresearch-fe.de>
2021-09-17 16:13:50 +0200

    hipos-vmstat: Show product name and machinfo
    Signed-off-by: Mario Schuknecht <mario.schuknecht@dresearch-fe.de>

--- recipes-hipos/hipos-vmstat/files/hipos-vmstat.sh
+++ recipes-hipos/hipos-vmstat/files/hipos-vmstat.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 wait_sec=30;
+PRODUCT=$(/usr/sbin/hip-productname)
+MACHINFO=$(/usr/sbin/hip-machinfo -a)
+FWVERSION=$(/usr/bin/lsb_release -d | rev | cut -d " " -f 1-2 | rev)
 
 while true
 do
 	/bin/sleep $((wait_sec))
-	/usr/bin/lsb_release -d | /usr/bin/logger -t vmstat
+	/usr/bin/logger -t vmstat "${PRODUCT} ${MACHINFO} ${FWVERSION}"
 	up_since=$(/usr/bin/uptime -s | tr -d "\n")
 	up_pretty=$(/usr/bin/uptime -p | tr -d "\n")
 	/usr/bin/logger -t vmstat "System up since $up_since $up_pretty"
