b81cb9dce995795f36578490e91c1d465bfde7f1
Mario Schuknecht <mario.schuknecht@dresearch-fe.de>
2021-11-19 09:26:03 +0100

    hipos-vmstat: Update version HYP-27399
    Output of 'lsb_release' has changed.
    
    Signed-off-by: Mario Schuknecht <mario.schuknecht@dresearch-fe.de>

--- recipes-hipos/hipos-vmstat/files/hipos-vmstat.sh
+++ recipes-hipos/hipos-vmstat/files/hipos-vmstat.sh
@@ -3,12 +3,13 @@
 wait_sec=30;
 PRODUCT=$(/usr/sbin/hip-productname)
 MACHINFO=$(/usr/sbin/hip-machinfo -a)
-FWVERSION=$(/usr/bin/lsb_release -d | rev | cut -d " " -f 1-2 | rev)
+FWVERSION=$(lsb_release -r | cut -f 2)
+FWCODENAME=$(lsb_release -c | cut -f 2)
 
 while true
 do
 	/bin/sleep $((wait_sec))
-	/usr/bin/logger -t vmstat "${PRODUCT} ${MACHINFO} ${FWVERSION}"
+	/usr/bin/logger -t vmstat "${PRODUCT} ${MACHINFO} ${FWVERSION} (${FWCODENAME})"
 	up_since=$(/usr/bin/uptime -s | tr -d "\n")
 	up_pretty=$(/usr/bin/uptime -p | tr -d "\n")
 	/usr/bin/logger -t vmstat "System up since $up_since $up_pretty"
