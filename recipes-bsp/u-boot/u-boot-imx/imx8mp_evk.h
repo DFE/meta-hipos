5791f575177ee5fd644c921fc37883e3b2efc758
Mario Schuknecht <mario.schuknecht@iris-sensing.com>
2026-03-27 16:30:28 +0100

    himx8: u-boot: Customize environment HYP-33833
    Enable booting from the eMMX by default.
    
    Signed-off-by: Mario Schuknecht <mario.schuknecht@iris-sensing.com>

--- recipes-bsp/u-boot/u-boot-imx/imx8mp_evk.h
+++ recipes-bsp/u-boot/u-boot-imx/imx8mp_evk.h
@@ -87,75 +87,24 @@
 #else
 #define CFG_EXTRA_ENV_SETTINGS		\
 	CFG_MFG_ENV_SETTINGS \
-	JAILHOUSE_ENV \
-	SR_IR_V2_COMMAND \
 	BOOTENV \
-	"prepare_mcore=setenv mcore_clk clk-imx8mp.mcore_booted;\0" \
-	"scriptaddr=0x43500000\0" \
 	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"bsp_script=boot.scr\0" \
-	"image=Image\0" \
-	"splashimage=0x50000000\0" \
-	"console=ttymxc1,115200\0" \
+	"do_boot=run switch_init; run loadimage; run loadfdt; run setbootargs; booti ${loadaddr} - ${fdt_addr}\0" \
+	"image=/boot/Image\0" \
 	"fdt_addr_r=0x43000000\0"			\
 	"fdt_addr=0x43000000\0"			\
-	"boot_fdt=try\0" \
 	"fdt_high=0xffffffffffffffff\0"		\
-	"boot_fit=no\0" \
-	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
-	"bootm_size=0x10000000\0" \
-	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=1\0" \
-	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
-	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot}\0 " \
-	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bsp_script};\0" \
-	"bootscript=echo Running bootscript from mmc ...; " \
-		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${fdtfile}\0" \
-	"mmcboot=echo Booting from mmc ...; " \
-		"run mmcargs; " \
-		"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-			"bootm ${loadaddr}; " \
-		"else " \
-			"if run loadfdt; then " \
-				"booti ${loadaddr} - ${fdt_addr_r}; " \
-			"else " \
-				"echo WARN: Cannot load the DT; " \
-			"fi; " \
-		"fi;\0" \
-	"netargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} " \
-		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-	"netboot=echo Booting from net ...; " \
-		"run netargs;  " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"${get_cmd} ${loadaddr} ${image}; " \
-		"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-			"bootm ${loadaddr}; " \
-		"else " \
-			"if ${get_cmd} ${fdt_addr_r} ${fdtfile}; then " \
-				"booti ${loadaddr} - ${fdt_addr_r}; " \
-			"else " \
-				"echo WARN: Cannot load the DT; " \
-			"fi; " \
-		"fi;\0" \
-	"bsp_bootcmd=echo Running BSP bootcmd ...; " \
-		"mmc dev ${mmcdev}; if mmc rescan; then " \
-		   "if run loadbootscript; then " \
-			   "run bootscript; " \
-		   "else " \
-			   "if run loadimage; then " \
-				   "run mmcboot; " \
-			   "else run netboot; " \
-			   "fi; " \
-		   "fi; " \
-	   "fi;"
+	"fdtfile=/boot/" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"switch_init=run switch_reset; mii device ethernet@30bf0000; mii write 0x15 1 0xc003; mii write 0x16 1 0xcfff\0" \
+	"switch_reset=gpio clear 97; sleep 0.1; gpio set 97; sleep 0.1\0" \
+	"loadimage=ext4load ${boottype} ${bootdev}:${bootpart} ${loadaddr} ${image}\0" \
+	"loadfdt=ext4load ${boottype} ${bootdev}:${bootpart} ${fdt_addr_r} ${fdtfile}\0" \
+	"setbootargs=setenv bootargs noinitrd console=ttymxc1,115200 " \
+		"root=${bootroot} rootwait\0" \
+	"x_bootA=setenv boottype mmc; setenv bootdev 2; setenv bootpart 1; " \
+		"setenv bootroot /dev/mmcblk2p1; run do_boot\0" \
+	"x_bootB=setenv boottype mmc; setenv bootdev 2; setenv bootpart 2; " \
+		"setenv bootroot /dev/mmcblk2p2; run do_boot\0"
 #endif
 
 /* Link Definitions */
